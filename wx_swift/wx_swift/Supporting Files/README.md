e5a05eee0dd91f97d64abccfe20df524f7a921bf
zane <17628048484@163.com>
2018-09-12 17:09:17 +0800

    the frist test project

---
+++ wx_swift/wx_swift/Supporting Files/README.md
@@ -0,0 +1,10 @@
+/* 
+  README.md
+  wx_swift
+
+  Created by Zane wang on 2018/9/4.
+  Copyright © 2018年 Zane wang. All rights reserved.
+*/
+
+## swift练习项目
+*练习swift项目架构实现了wx从启动到内部部分逻辑的实现*
---
+++ wx_swift/wx_swiftTests/wx_swiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  wx_swiftTests.swift
+//  wx_swiftTests
+//
+//  Created by Zane wang on 2018/9/4.
+//  Copyright © 2018年 Zane wang. All rights reserved.
+//
+
+import XCTest
+@testable import wx_swift
+
+class wx_swiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
